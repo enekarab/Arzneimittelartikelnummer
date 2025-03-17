23fc6c0c4b689e2a1fb1cd422a0580a05d704136
enekarab <116077816+enekarab@users.noreply.github.com>
2025-03-17 14:07:44 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function isValidPZN() {
+    var input = document.getElementById("PZN").value.trim();
+
+    if (isNaN(input)) {
+        alert("Bitte nur Zahlen eingeben!");
+        return;
+    } else if (input.length < 7) {
+        alert("Die eingegebene Zahl hat weniger als 7 Ziffern!");
+        return;
+    }
+
+
+
+    var sum = (input[0] * 3) + (input[1] * 1) + (input[2] * 3) + (input[3] * 1) + (input[4] * 3) + (input[5] * 1);
+
+    var rest = (11 - (sum % 11)) % 11;
+
+    var output = document.getElementById("output");
+    if (rest == input[6]) {
+        output.innerHTML = "Die PZN " + input + " ist gültig.";
+
+        var barcode = document.createElement("img");
+        barcode.src = "https://barcode.tec-it.com/barcode.ashx?data=" + input + "&code=Code128&translate-esc=on";
+        output.appendChild(barcode);
+    
+    } else {
+        output.innerHTML = "Die PZN " + input + " ist ungültig. Erwartete Prüfziffer: " + rest;
+    }
+}
